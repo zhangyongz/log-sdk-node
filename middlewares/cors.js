fa1bd17703cbb68dc10d5c07286613d6c0ad4a74
zhangyong <zhangyong_web@163.com>
2021-01-21 18:03:11 +0800

    edit cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,5 @@
 const cors = async (ctx, next) => {
-  ctx.set("Access-Control-Allow-Origin", "*")
+  ctx.set("Access-Control-Allow-Origin", ctx.headers.origin || "*")
   ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
   ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type, token, Authorization")
   ctx.set("Content-Type", "application/json;charset=utf-8")
